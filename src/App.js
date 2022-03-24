a4494581659c8f80f245f607477a5a6e2072cd4c
hhamza1 <hamzahmitou@gmail.com>
2022-03-24 14:54:38 +0300

    Refactored the app to map data to component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 
-import { ReportSorter , ImgContainer} from './components/StyledComponents';
+import { ReportSorter , ImgContainer, ReportContainer} from './components/StyledComponents';
 
 import ProfilePic from './assets/image-jeremy.png';
+import Work from './assets/icon-work.svg';
+import Study from './assets/icon-study.svg';
+import Ellipsis from './assets/icon-ellipsis.svg';
+import Play from './assets/icon-play.svg';
+import Exercise from './assets/icon-exercise.svg';
+import SelfCare from './assets/icon-self-care.svg';
+import Social from './assets/icon-social.svg';
+
+import data from './data/data.json';
 
 
 import './App.scss';
 
 function App() {
+
+  const handleIcon = (icon) =>{
+    switch(icon) {
+      case "Work":
+        return Work;
+      case "Play":
+        return Play;
+      case "Study":
+        return Study;
+      case "Exercise":
+        return Exercise;
+      case "Social":
+        return Social;
+      case "Self Care":
+        return SelfCare;
+      default:
+        return ;
+    }
+  }
+
+
+
+
+
+
+
   return (
     <div className="App">
       <ReportSorter>
@@ -24,6 +59,28 @@ function App() {
             <button className='sorting-btn'>Monthly</button>
           </div>
       </ReportSorter>
+      {
+        data.map((el, index) => {
+          return (
+            <ReportContainer 
+              index={index} 
+              color={el.title} 
+              icon={handleIcon(el.title)}
+            >
+            <div className="tracking-details">
+              <div className='top-section'>
+                <h3>{el.title}</h3>
+                <img src={Ellipsis} alt="test"/>
+              </div>
+              <div className='bottom-section'>
+                <h2>{el.timeframes.weekly.current}hrs</h2>
+                <p>Last Week - {el.timeframes.weekly.previous}hrs</p>
+              </div>
+            </div>
+            </ReportContainer>
+          )
+        })
+      }
     </div>
   );
 }
